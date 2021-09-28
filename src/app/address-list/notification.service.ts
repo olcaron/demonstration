1865622b5b8da34860dd74d481144d5ab7bfd45f
Mika Rafaralahy <mrafa099@uottawa.ca>
2021-09-28 09:04:16 -0500

    message

---
+++ src/app/address-list/notification.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {AddressEntry} from "./address-entry";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  //Observable for selected elements
+  selectedElement = new BehaviorSubject<AddressEntry>(null);
+  constructor() { }
+
+  public selectionChanged(address: AddressEntry): void {
+    this.selectedElement.next(address);
+  }
+}
